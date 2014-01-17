423e2b6c0320a3bcd53906f05fca7e89e2f27af1
Mark Borcherding <markborcherding@gmail.com>
2014-01-16 20:28:40 -0600

    remove deprecation warnings

--- spec/dummy/config/initializers/secret_token.rb
+++ spec/dummy/config/initializers/secret_token.rb
@@ -5,3 +5,4 @@
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
 Dummy::Application.config.secret_token = '61f37f47408da6ef2529d12e92bc451a57b1c10c069d782477fd6248e7d011deb5731e335a9fb2dce2f6a22b0809b0173da8b3b2f169049ea0cead9b3c0d0b84'
+Dummy::Application.config.secret_key_base = '61f37f47408da6ef2529d12e92bc451a57b1c10c069d782477fd6248e7d011deb5731e335a9fb2dce2f6a22b0809b0173da8b3b2f169049ea0cead9b3c0d0b84'
